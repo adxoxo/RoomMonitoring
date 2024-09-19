728f2e30c6716ec45f40a96c37b167d0c22fccff
adxoxo <adamgemenez@gmail.com>
2024-09-20 03:31:48 +0800

    setup database models

--- Software/Backend/server/roomapi/admin.py
+++ Software/Backend/server/roomapi/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Room, RoomParameters
 
+admin.site.register(Room)
+admin.site.register(RoomParameters)
 # Register your models here.
--- Software/Backend/server/roomapi/models.py
+++ Software/Backend/server/roomapi/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=200, null=True)    
+    device_id = models.CharField(max_length=200, null=True)
+    listed = models.BooleanField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+class RoomParameters(models.Model):
+    temp = models.FloatField(null=True)
+    humidity = models.FloatField(null=True)
+    air_quality = models.FloatField(null=True)
+    dust_ppm = models.FloatField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    
